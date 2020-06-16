45b9310c5fd62328431affb7532c3f073b67aa8d
Remy Derollez <derollez@stanford.edu>
2020-06-16 12:25:05 -0700

    list of TODOs for future work

---
+++ src/TODO.jl
@@ -0,0 +1,12 @@
+#TODO document
+
+
+#PCE
+#Remove PCEPorpagator.f and interface with TrajOpt and rollout
+#See if TrajOpt can handle non-autonomous systems and see how to deal with it
+#get nbr steps into the propagator (reused many times)
+#check other density estimation methods for reconstruction after compute coefficients
+#Try to use the already generated ξ to do the reconstruction job
+#Adding other visualization tools (projected ellipsoids plots)
+
+#Next Step is Set-based Unscented Propagation
